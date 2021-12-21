8caa1154b4789433997ad8e40fd6a021d9a61595
Piotr Kalachyn <pkalachyn@griddynamics.com>
2021-12-21 13:42:14 +0000

    Edge case js task 85 validate bets

--- js-practice/tasks/85_validate-bet/validate-bet.spec.js
+++ js-practice/tasks/85_validate-bet/validate-bet.spec.js
@@ -9,6 +9,14 @@ describe.skip('validateBet', () => {
     expect(validateBet([5, 90], '5 , 3, 1  4,2').join(',')).toBe('1,2,3,4,5');
   });
 
+  it('validateBet "5 , 37, 12  4, 2" should return [1, 2, 3, 4, 5]', () => {
+    expect(validateBet([5, 90], '5 , 37, 12  4,2').join(',')).toBe('2,4,5,12,37');
+  });
+
+  it('validateBet "5 , 3, 1  4, 1" with non-unique bets should return null', () => {
+    expect(validateBet([5, 90], '5 , 3, 1  4,2').join(',')).toBeNull();
+  });
+
   it('validateBet "1, 2; 3, 4, 5" should return null', () => {
     expect(validateBet([5, 90], '1, 2; 3, 4, 5')).toBeNull();
   });
