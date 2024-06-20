abfb2b7503f111cb150d8c38a059afaa3bec6d7a
Patryk Sąsiadek <psasiadek@griddynamics.com>
2024-06-20 18:25:42 +0200

    13th task completed

---
+++ js-practice/tasks/13_largest-of-four/largest-of-four.js
@@ -0,0 +1,5 @@
+const largestOfFour = (arr) => {
+  const largestNumbers = arr.map((subArray) => subArray.reduce((acc, current) => (current >= acc ? current : acc), 0));
+
+  return largestNumbers;
+};
