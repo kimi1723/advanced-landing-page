22fabf4772527a73fbca5b533dad294ea73e1479
Patryk Sąsiadek <psasiadek@griddynamics.com>
2024-06-20 13:45:44 +0200

    5th task lookup-profile completed

---
+++ js-practice/tasks/5_lookup-profile/lookup-profile.js
@@ -0,0 +1,39 @@
+const contacts = [
+    {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"],
+      "points": 0
+    },
+    {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+  ];
+
+const lookupProfile = (firstName, prop) => {
+    const contactIndex = contacts.findIndex(contact => contact.firstName === firstName);
+
+    if(contactIndex === -1) return 'No such contact';
+
+    const contactProp = contacts[contactIndex][prop];
+
+    if(contactProp === undefined) return 'No such property';
+
+    return contactProp;
+};
