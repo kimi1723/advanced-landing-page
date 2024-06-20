1d94f64bf6ea8ae1d3138ffb0cb31abd1041106d
Patryk Sąsiadek <psasiadek@griddynamics.com>
2024-06-20 17:42:04 +0200

    fixed variable name

--- js-practice/tasks/11_find-longest-word/find-longest-word.js
+++ js-practice/tasks/11_find-longest-word/find-longest-word.js
@@ -1,5 +1,5 @@
-const findLongestWord = (word) => {
-  const words = word.split(' ');
+const findLongestWord = (str) => {
+  const words = str.split(' ');
   const longestWordLength = words.reduce((acc, current) => (current.length > acc ? current.length : acc), 0);
 
   return longestWordLength;
