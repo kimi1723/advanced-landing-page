300cb59c311f64a238902c262fad74d4def2b5cf
Patryk Sąsiadek <psasiadek@griddynamics.com>
2024-06-21 11:01:48 +0200

    run the test

--- js-practice/tasks/11_find-longest-word/find-longest-word.js
+++ js-practice/tasks/11_find-longest-word/find-longest-word.js
@@ -1,4 +1,4 @@
-const findLongestWord = (str) => {
+export const findLongestWord = (str) => {
   const words = str.split(' ');
   const longestWordLength = words.reduce((acc, current) => (current.length > acc ? current.length : acc), 0);
 
