3bc0618d1741f2b4a42d5ae3fb92417fe138859e
Patryk Sąsiadek <psasiadek@griddynamics.com>
2024-06-21 12:27:57 +0200

    run the test

--- js-practice/tasks/13_largest-of-four/largest-of-four.js
+++ js-practice/tasks/13_largest-of-four/largest-of-four.js
@@ -1,4 +1,4 @@
-const largestOfFour = (arr) => {
+export const largestOfFour = (arr) => {
   const largestNumbers = arr.map((subArray) => subArray.reduce((acc, current) => (current >= acc ? current : acc), 0));
 
   return largestNumbers;
