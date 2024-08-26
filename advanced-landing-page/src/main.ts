0008fa0fc36b127286f360df330c0dffbcc661cb
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-08-26 20:38:08 +0200

    iniy

---
+++ advanced-landing-page/src/main.ts
@@ -0,0 +1,9 @@
+import { handlePropertyList } from "./ts/handlePropertyList";
+import { handleProperties } from "./ts/handleProperties";
+import { handleVideo } from "./ts/handleVideo";
+import { handleReviews } from "./ts/handleReviews";
+
+handlePropertyList();
+handleProperties();
+handleVideo();
+handleReviews();
---
+++ advanced-landing-page/src/ts/handlePropertyList.ts
@@ -0,0 +1,51 @@
+const propertyList = document.querySelector(
+  ".property-list"
+) as HTMLUListElement;
+const propertyBtn = document.querySelector(
+  ".nav__item-btn"
+) as HTMLButtonElement;
+
+let isVisible = false;
+
+const listVisibilityHandler = async () =>
+  new Promise((res) => {
+    isVisible = !propertyList.classList.toggle("property-list--hidden");
+
+    propertyList.classList.contains("hidden")
+      ? res(propertyList.classList.remove("hidden"))
+      : setTimeout(() => res(propertyList.classList.add("hidden")), 300);
+  });
+
+const documentListener = (e: Event) => {
+  const target = e.target;
+
+  if (
+    !isVisible ||
+    !(target instanceof HTMLElement) ||
+    target.closest(".nav__item-btn") === propertyBtn
+  )
+    return;
+
+  listVisibilityHandler();
+};
+
+const propertyListHandler = async () => {
+  let isRunning = false;
+
+  return async () => {
+    if (isRunning) return;
+
+    isRunning = true;
+
+    await listVisibilityHandler();
+
+    isRunning = false;
+  };
+};
+
+export const handlePropertyList = async () => {
+  const handler = await propertyListHandler();
+  propertyBtn.addEventListener("click", handler);
+
+  document.addEventListener("click", documentListener);
+};
