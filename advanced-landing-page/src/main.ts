60c2daa738c59d24b4875549122a7432108897ec
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2024-09-02 11:37:18 +0200

    Delete advanced-landing-page directory

--- advanced-landing-page/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { handlePropertyList } from "./ts/handlePropertyList";
-import { handleProperties } from "./ts/handleProperties";
-import { handleVideo } from "./ts/handleVideo";
-import { handleReviews } from "./ts/handleReviews";
-
-handlePropertyList();
-handleProperties();
-handleVideo();
-handleReviews();
